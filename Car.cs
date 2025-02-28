4c891b81a56d9e0dd1e0e2f8fde769d4dfa011e9
Kab-35-16 <Kab-35-16@KAB-31-9->
2025-02-28 12:51:13 +0200

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR26
+{
+    class Car
+    {
+        string model;
+        int year;
+        public static int totalCars = 0;
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+        public Car() {}
+        public Car(string model,int Year)
+        {
+            this.Model = model;
+            this.Year = year;
+            totalCars++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car avto = new avto();
+            avto.Model = "BMW";
+            avto.Year = 888;
+            Console.WriteLine(avto);
+
+            Car avto1 = new Car("Audi", 1990);
+            Car avto2 = new Car("BMW", 2007);
+            Car avto3 = new Car("Nisan", 2000);
+            Car avto4 = new Car("Chevrolet Camaro", 1996);
+            Car avto5 = new Car("Lexus", 1999);
+            Console.WriteLine($"Марка {avto.Model} Год Производства {avto1.Year}");
+            Console.WriteLine($"Марка {avto.Model} Год Производства {avto2.Year}");
+            Console.WriteLine($"Марка {avto.Model} Год Производства {avto3.Year}");
+            Console.WriteLine($"Марка {avto.Model} Год Производства {avto4.Year}");
+            Console.WriteLine($"Марка {avto.Model} Год Производства {avto5.Year}");
+            Console.ReadKey();
+        }
+    }
+}
